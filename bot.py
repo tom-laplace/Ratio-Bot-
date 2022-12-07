c03d8c12e51f851468995152568a88c934a57bf2
Tom Laplace <tomlaplace0902@gmail.com>
2022-12-07 11:29:24 +0100

    Ratio Bot v1.0.1 (commenté)

--- bot.py
+++ bot.py
@@ -1,3 +1,8 @@
+# Pour utiliser ce code vous devez aller sur Discord Developer Portal et créer une application
+# Vous devez ensuite créer un bot et récupérer son token
+# Vous devez ensuite créer un fichier secret.py et y mettre la ligne suivante: BOT_TOKEN = 'token' 
+# Dans le terminal, vous devez installer discord.py avec la commande suivante: pip install discord.py
+
 import discord
 from discord.ext import commands
 from secret import BOT_TOKEN
@@ -10,14 +15,14 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    # List of keywords to trigger the bot
+    # Liste des mots qui activent le bot
     keywords = ['macron', 'aigri', 'égri', 'égris', 'ai gri', 'ai gris', ' é gri', 'aigry', 'égry', 'gris']
     if any(word in message.content.lower() for word in keywords):
-        # Send a message to the channel (you can change it easily)
+        # Envoie un message dans le channel
         await message.channel.send('ratio + palu')
 
-# Run the bot 
-# BOT_TOKEN is in secret.py
+# Lance le bot
+# BOT_TOKEN est dans le fichier secret.py (crée plus haut)
 bot.run(BOT_TOKEN)
 
-# RUN python bot.py in your terminal to launch the bot
+# Exécuter python bot.py dans le terminal pour lancer le bot
